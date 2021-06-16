63a17f7767adddda101645d52c01e4872b73ae31
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-16 19:36:57 +0900

    refactor: tweets 라우터

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import crypto from 'crypto';
+import 'express-async-errors';
 
 const router = express.Router();
 
@@ -9,67 +9,39 @@ let tweets = [
     text: 'tweet message1',
     username: 'user1',
     name: 'tom',
-    createdAt: new Date('2021-06-01'),
+    createdAt: new Date('2021-06-01').toString(),
   },
   {
     id: '124',
     text: 'tweet message2',
     username: 'user2',
     name: 'bob',
-    createdAt: new Date('2021-06-15'),
+    createdAt: new Date('2021-06-15').toString(),
   },
   {
     id: '125',
     text: 'tweet message3',
     username: 'user3',
     name: 'anne',
-    createdAt: new Date('2021-05-15'),
+    createdAt: new Date('2021-05-15').toString(),
   },
 ];
 
 router.get('/', (req, res) => {
-  if (req.query.username) {
-    // username으로 특정 user가 작성한 모든 트윗 조회
-    const filtered = tweets.filter((tweet) => tweet.username === req.query.username);
-
-    if (filtered.length > 0) {
-      res.status(200).json(filtered);
-    } else {
-      const error = {
-        result: 'error',
-        error: {
-          name: 'NotFoundError',
-          message: 'Any tweet is not found, written by this user',
-          code: 404,
-        },
-      };
-      res.status(404).json(error);
-    }
-  } else {
-    // 모든 트윗 조회
-    res.status(200).json(tweets);
-  }
+  const username = req.query.username;
+  const data = username ? tweets.filter((tweet) => tweet.username === username) : tweets;
+  res.status(200).json(data);
 });
 
 router.get('/:id', (req, res) => {
-  // id로 특정 트윗 조회
   const id = req.params.id;
-  const filtered = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweets.find((tweet) => tweet.id === id);
 
-  if (filtered) {
-    res.status(200).json(filtered);
+  if (tweet) {
+    res.status(200).json(tweet);
   } else {
-    const error = {
-      result: 'error',
-      error: {
-        name: 'NotFoundError',
-        message: 'Any tweet is not found, matched with this id',
-        code: 404,
-      },
-    };
-    res.status(404).json(error);
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
   }
-  res.send('Get /tweets/:id');
 });
 
 function tweetValidator(tweet) {
@@ -79,78 +51,38 @@ function tweetValidator(tweet) {
 }
 
 router.post('/', (req, res) => {
-  // 트윗 생성
-  const tweet = req.body;
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createAt: new Date(),
+    name,
+    username,
+  };
 
-  if (tweetValidator(tweet)) {
-    const id = crypto.randomBytes(16).toString('hex');
-    const createdAt = new Date();
-
-    const newTweet = { id, createdAt, ...tweet };
-
-    tweets.push(newTweet);
-    res.status(201).json(newTweet);
-  } else {
-    const error = {
-      result: 'error',
-      error: {
-        name: 'ValidationError',
-        message: 'A invalid input is submitted',
-        code: 400,
-      },
-    };
-    res.status(400).json(error);
-  }
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
 });
 
 router.put('/:id', (req, res) => {
   // 트윗 수정
   const id = req.params.id;
-  const input = req.body;
-  const target = tweets.find((tweet) => tweet.id === id);
+  const { text } = req.body;
+  const tweet = tweets.find((tweet) => tweet.id === id);
 
-  if (target) {
-    let updated;
-
-    tweets.map((tweet) => {
-      if (tweet.id !== id) return tweet;
-      updated = { ...tweet, ...input };
-      return updated;
-    });
-
-    res.status(200).json(updated);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
   } else {
-    const error = {
-      result: 'error',
-      error: {
-        name: 'NotFoundError',
-        message: 'Any tweet is not found, matched with this id',
-        code: 404,
-      },
-    };
-    res.status(404).json(error);
+    res.status(404).json({ message: `Tweet id(${id}) not found` });
   }
 });
 
 router.delete('/:id', (req, res) => {
   // 트윗 삭제
   const id = req.params.id;
-  const target = tweets.find((tweet) => tweet.id === id);
-
-  if (target) {
-    tweets = tweets.filter((tweet) => tweet.id !== id);
-    res.sendStatus(204);
-  } else {
-    const error = {
-      result: 'error',
-      error: {
-        name: 'NotFoundError',
-        message: 'Any tweet is not found, matched with this id',
-        code: 404,
-      },
-    };
-    res.status(404).json(error);
-  }
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
 });
 
 export default router;
