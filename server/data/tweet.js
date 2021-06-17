cdebfd280317806cd72a6dbcdbda4506a76bd875
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-17 23:33:27 +0900

    feat: controller 레이어 수정

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -22,19 +22,19 @@ let tweets = [
   },
 ];
 
-export function getAll() {
+export async function getAll() {
   return tweets;
 }
 
-export function getAllByUsername(username) {
+export async function getAllByUsername(username) {
   return tweets.filter((tweet) => tweet.username === username);
 }
 
-export function getById(id) {
+export async function getById(id) {
   return tweets.find((tweet) => tweet.id === id);
 }
 
-export function create(text, name, username) {
+export async function create(text, name, username) {
   const tweet = {
     id: Date.now().toString(),
     text,
@@ -46,7 +46,7 @@ export function create(text, name, username) {
   return [tweet, ...tweets];
 }
 
-export function update(id, text) {
+export async function update(id, text) {
   const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
     tweet.text = text;
@@ -54,6 +54,6 @@ export function update(id, text) {
   return tweet;
 }
 
-export function removes(id) {
+export async function remove(id) {
   return tweets.filter((tweet) => tweet.id !== id);
 }
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,54 +1,22 @@
 import express from 'express';
 import 'express-async-errors';
-import controller from '../controller/tweet.js';
-import * as tweetRepository from '../data/tweet.js';
+import * as tweetController from '../controller/tweet.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const username = req.query.username;
-  const data = username
-    ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll();
-  res.status(200).json(data);
-});
+// GET /tweets
+router.get('/', tweetController.getTweets);
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  const tweet = tweetRepository.getById(id);
+// GET /tweets/:id
+router.get('/:id', tweetController.getTweet);
 
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
-  }
-});
+// POST /tweets
+router.post('/', tweetController.createTweet);
 
-router.post('/', (req, res) => {
-  const { text, name, username } = req.body;
-  const tweet = tweetRepository.create(text, name, username);
+// PUT /tweets/:id
+router.put('/:id', tweetController.updateTweet);
 
-  res.status(201).json(tweet);
-});
-
-router.put('/:id', (req, res) => {
-  // 트윗 수정
-  const id = req.params.id;
-  const { text } = req.body;
-  const tweet = tweetRepository.update(id, text);
-
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
-  }
-});
-
-router.delete('/:id', (req, res) => {
-  // 트윗 삭제
-  const id = req.params.id;
-  tweets = tweetRepository.remove(id);
-  res.sendStatus(204);
-});
+// DELETE /tweets/:id
+router.delete('/:id', tweetController.deleteTweet);
 
 export default router;
