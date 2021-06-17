92f0366c443bebc8f40692c2aaf23c24eaa617e7
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-17 23:47:17 +0900

    refactor: client 통신 로직 개선

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -2,71 +2,52 @@ export default class TweetService {
   constructor(baseURL) {
     this.baseURL = baseURL;
   }
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
 
   async getTweets(username) {
     const query = username ? `?username=${username}` : '';
-    const response = await fetch(`${this.baseURL}/tweets${query}`, {
+    return requestToServer({
+      url: `${this.baseURL}/tweets${query}`,
       method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
     });
-
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-
-    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`, {
+    return requestToServer({
+      url: `${this.baseURL}/tweets`,
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      data: JSON.stringify({ text, username: 'dan', name: 'Dan' }),
+      body: JSON.stringify({ text, username: 'dan', name: 'Dan' }),
+      errorCode: 201,
     });
-
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return requestToServer({
+      url: `${this.baseURL}/tweets/${tweetId}`,
       method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
+      errorCode: 204,
     });
-
-    const data = await response.json();
-    if (response.status !== 204) {
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return requestToServer({
+      url: `${this.baseURL}/tweets/${tweetId}`,
       method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      data: JSON.stringify({ text }),
+      body: JSON.stringify({ text }),
     });
-
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-
-    return data;
   }
+}
+
+async function requestToServer({ url, method, body, errorCode = 200 }) {
+  const response = await fetch(url, {
+    method: method,
+    headers: { 'Content-Type': 'application/json' },
+    data: body || undefined,
+  });
+
+  const data = await response.json();
+  if (response.status !== errorCode) {
+    throw new Error(data.message);
+  }
+
+  return data;
 }
