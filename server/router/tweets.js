25249c8d011020a2971d2e0939a48f654e9d1986
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-18 20:51:24 +0900

    refactor: 유효성 검사 수정

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,17 +1,14 @@
 import express from 'express';
-import { body, query, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import 'express-async-errors';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    return next();
-  }
-  return res.status(400).json({ message: errors.array()[0].msg });
-};
+const validateTweet = [
+  body('text').trim().isLength({ min: 3 }).withMessage('text should be at least 3 charactors'),
+];
 
 // GET /tweets
 router.get('/', tweetController.getTweets);
@@ -20,25 +17,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post(
-  '/',
-  [
-    body('text').isLength({ min: 2 }).withMessage('text have to be longger than 2'),
-    body(['username', 'name'])
-      .notEmpty()
-      .withMessage('username and name have not to be empty'),
-  ],
-  validate,
-  tweetController.createTweet
-);
+router.post('/', validateTweet, validate, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put(
-  '/:id',
-  body('text').isLength({ min: 2 }).withMessage('text have to be longger than 2'),
-  validate,
-  tweetController.updateTweet
-);
+router.put('/:id', validateTweet, validate, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
