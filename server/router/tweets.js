8b1b72a43a4940d24bccbcfde0147780af9b3ca1
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-18 20:43:46 +0900

    feat: 유효성 검사 추가

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import { body, query, validationResult } from 'express-validator';
 import 'express-async-errors';
 import * as tweetController from '../controller/tweet.js';
 
 const router = express.Router();
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
 
 // GET /tweets
 router.get('/', tweetController.getTweets);
@@ -11,10 +20,25 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post(
+  '/',
+  [
+    body('text').isLength({ min: 2 }).withMessage('text have to be longger than 2'),
+    body(['username', 'name'])
+      .notEmpty()
+      .withMessage('username and name have not to be empty'),
+  ],
+  validate,
+  tweetController.createTweet
+);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put(
+  '/:id',
+  body('text').isLength({ min: 2 }).withMessage('text have to be longger than 2'),
+  validate,
+  tweetController.updateTweet
+);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
